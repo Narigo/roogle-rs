cb6cbb3add3df1d936904100aa0a041c3f36042b
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-01-25 10:15:02 +0100

    Begin setting up new initial screen
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/main.rs
+++ src/main.rs
@@ -9,31 +9,35 @@ use std::time::Duration;
 struct MyDataModel;
 
 impl Layout for MyDataModel {
-    fn layout(&self, info: WindowInfo<Self>) -> Dom<Self> {
-        Dom::div().with_id("wrapper")
-            .with_child(Dom::label("Hello123").with_id("red"))
-            .with_child(Dom::div().with_id("sub-wrapper")
-                .with_child(Dom::div().with_id("yellow")
-                    .with_child(Dom::div().with_id("orange-1"))
-                    .with_child(Dom::div().with_id("orange-2"))
-                )
-                .with_child(Dom::div().with_id("grey"))
-            )
-            .with_child((0..50).map(|i| Dom::label(format!("{}", i))).collect::<Dom<Self>>().with_id("rows"))
+    fn layout(&self, _info: WindowInfo<Self>) -> Dom<Self> {
+        Dom::div().with_id("main").with_child(
+            Dom::div()
+                .with_id("head")
+                .with_child(Dom::label("Welcome to Roogle")),
+        )
     }
 }
 
 fn main() {
+    macro_rules! CSS_PATH {
+        () => {
+            concat!(env!("CARGO_MANIFEST_DIR"), "/src/main.css")
+        };
+    }
 
-    macro_rules! CSS_PATH { () => (concat!(env!("CARGO_MANIFEST_DIR"), "/src/main.css")) }
-
-    let mut app = App::new(MyDataModel, AppConfig::default());
+    let app = App::new(MyDataModel, AppConfig::default());
 
     #[cfg(debug_assertions)]
-    let window = Window::new_hot_reload(WindowCreateOptions::default(), css::hot_reload(CSS_PATH!(), Duration::from_millis(500))).unwrap();
+    let window = Window::new_hot_reload(
+        WindowCreateOptions::default(),
+        css::hot_reload(CSS_PATH!(), Duration::from_millis(500)),
+    ).unwrap();
 
     #[cfg(not(debug_assertions))]
-    let window = Window::new(WindowCreateOptions::default(), css::from_str(include_str!(CSS_PATH!())).unwrap()).unwrap();
+    let window = Window::new(
+        WindowCreateOptions::default(),
+        css::from_str(include_str!(CSS_PATH!())).unwrap(),
+    ).unwrap();
 
     app.run(window).unwrap();
 }
