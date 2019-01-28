8af3383d26a023e365de638014f99ee237a965e5
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-01-28 02:42:50 +0100

    Use web-view for GUI
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/frontend/component/app/app.js
+++ src/frontend/component/app/app.js
@@ -9,7 +9,14 @@ const App = () => {
         <h1>Welcome to Roogle.</h1>
         <p>This is a description about roogle.</p>
       </header>
-      <div className={style.dropfile}>Drop a PDF file here</div>
+      <div
+        className={style.dropfile}
+        onClick={() => {
+          window.external.invoke(JSON.stringify({ cmd: "log", text: "hello click!" }));
+        }}
+      >
+        Drop a PDF file here
+      </div>
     </div>
   );
 };
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,34 @@
 #![windows_subsystem = "windows"]
 
-extern crate azul;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+extern crate web_view;
 
-use azul::prelude::*;
-#[cfg(debug_assertions)]
-use std::time::Duration;
-
-struct MyDataModel;
-
-impl Layout for MyDataModel {
-    fn layout(&self, _info: WindowInfo<Self>) -> Dom<Self> {
-        Dom::div()
-            .with_id("main")
-            .with_child(
-                Dom::div()
-                    .with_id("head")
-                    .with_child(Dom::label("Welcome to Roogle").with_class("headline"))
-                    .with_child(
-                        Dom::label("This is a description about roogle").with_class("description"),
-                    ),
-            ).with_child(
-                Dom::div()
-                    .with_id("processor")
-                    .with_child(Dom::label("Drag a text file here to test it!"))
-                    .with_callback(On::MouseUp, Callback(|m: &mut AppState<MyDataModel>, _| {
-                        println!("clicked on drag here");
-                        Redraw
-                    }))
-            )
-    }
-}
+use web_view::*;
 
 fn main() {
-    macro_rules! CSS_PATH {
-        () => {
-            concat!(env!("CARGO_MANIFEST_DIR"), "/src/main.css")
-        };
-    }
+    web_view::builder()
+        .title("Roogle")
+        .content(Content::Html(include_str!("../dist/index.html")))
+        .size(800, 600)
+        .resizable(true)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, arg| {
+            use Cmd::*;
+            match serde_json::from_str(arg).unwrap() {
+                Init => (),
+                Log { text } => println!("{}", text),
+            }
+            Ok(())
+        }).run()
+        .unwrap();
+}
 
-    let app = App::new(MyDataModel, AppConfig::default());
-
-    #[cfg(debug_assertions)]
-    let window = Window::new_hot_reload(
-        WindowCreateOptions::default(),
-        css::hot_reload(CSS_PATH!(), Duration::from_millis(500)),
-    ).unwrap();
-
-    #[cfg(not(debug_assertions))]
-    let window = Window::new(
-        WindowCreateOptions::default(),
-        css::from_str(include_str!(CSS_PATH!())).unwrap(),
-    ).unwrap();
-
-    app.run(window).unwrap();
+#[derive(Deserialize)]
+#[serde(tag = "cmd", rename_all = "camelCase")]
+pub enum Cmd {
+    Init,
+    Log { text: String },
 }
