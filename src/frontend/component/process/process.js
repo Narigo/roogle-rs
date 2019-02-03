80fba0c6d1f1e8fa675c45083b8d712b288d0067
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-02-03 23:44:04 +0100

    Try and test useEffect for processing
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/frontend/component/process/process.js
+++ src/frontend/component/process/process.js
@@ -1,9 +1,96 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./process.scss";
 
 const Process = ({ files }) => {
   const [sentences, setSentences] = useState(null);
-  return <div className={style.root}>{!sentences ? <div>Processing files {files}</div> : <div>{sentences}</div>}</div>;
+  const [progress, setProgress] = useState("start");
+
+  useEffect(() => {
+    let cancelled = false;
+    writeTmpFile()
+      .then(file => {
+        if (!cancelled) {
+          setProgress("tmp-file-created");
+          tmpFile = file;
+          return readPdfIntoText(file);
+        } else {
+          throw new Error("cancelled");
+        }
+      })
+      .then(text => {
+        if (!cancelled) {
+          setProgress("process-text");
+          return splitTextIntoSentences(text);
+        } else {
+          throw new Error("cancelled");
+        }
+      })
+      .then(sentences => {
+        if (!cancelled) {
+          setProgress("remove-tmp-file");
+          return removeTmpFile(tmpFile).then(() => sentences);
+        } else {
+          throw new Error("cancelled");
+        }
+      })
+      .then(sentences => {
+        if (!cancelled) {
+          setProgress("done");
+          return setSentences(sentences);
+        } else {
+          throw new Error("cancelled");
+        }
+      })
+      .catch(error => {
+        console.log("catched!");
+        if (error.message === "cancelled") {
+          if (tmpFile) {
+            return removeTmpFile(tmpFile);
+          }
+        }
+      });
+    return () => {
+      cancelled = true;
+    };
+  }, [files]);
+  return (
+    <div className={style.root}>
+      {progress === "start" ? (
+        <div>Creating temp file from binary in Rust</div>
+      ) : progress === "tmp-file-created" ? (
+        <div>temp file created, reading PDF</div>
+      ) : progress === "process-text" ? (
+        <div>Selecting sentences</div>
+      ) : (
+        <div>{sentences}</div>
+      )}
+    </div>
+  );
 };
 
 export default Process;
+
+function writeTmpFile() {
+  console.log(`called writeTmpFile()`);
+  return new Promise(resolve => {
+    setTimeout(() => resolve("/tmp/someFile"), 2000);
+  });
+}
+function readPdfIntoText(file) {
+  console.log(`called readPdfIntoText(${file})`);
+  return new Promise(resolve => {
+    setTimeout(() => resolve("This is a test. Hello, good morning! Another test sentence."), 2000);
+  });
+}
+function splitTextIntoSentences(text) {
+  console.log(`called splitTextIntoSentences(${text})`);
+  return new Promise(resolve => {
+    setTimeout(() => resolve(["This is a test", "Hello good morning", "Another test sentence"]), 2000);
+  });
+}
+function removeTmpFile(tmpFile) {
+  console.log(`called removeTmpFile(${tmpFile})`);
+  return new Promise(resolve => {
+    setTimeout(resolve, 2000);
+  });
+}
