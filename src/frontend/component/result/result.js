fb5f6c19a4c0918976370daa3891f461f66f6016
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-02-16 17:57:44 +0100

    Try another way to get something
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/frontend/component/result/result.js
+++ src/frontend/component/result/result.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./result.scss";
-import useRustCommand from "../../hooks/use-rust-command";
+// import useRustCommand from "../../hooks/use-rust-command";
 
 const Result = ({ sentences }) => {
-  const openUrl = useRustCommand("openUrl");
+  // const openUrl = useRustCommand("openUrl");
+  const [result, setResult] = useState(null);
   useEffect(() => {
-    openUrl(`https://www.google.de/search?q=${encodeURIComponent(sentences[0])}`);
+    const url = `https://www.google.de/search?q=${encodeURIComponent(sentences[0])}`;
+    fetch(url, { mode: "no-cors" })
+      .then(res => res.body())
+      .then(setResult);
   });
-  return <div className={style.root}>This is the result: {sentences}</div>;
+  return <div className={style.root}>{result === null ? "Waiting for result" : result}</div>;
 };
 
 export default Result;
