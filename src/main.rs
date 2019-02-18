01f839db9c0761f6991f757135ee8e7a96848b36
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-02-18 11:21:21 +0100

    Remove old code
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/main.rs
+++ src/main.rs
@@ -42,25 +42,4 @@ fn fetch_url<T>(wv: &mut WebView<T>, url: String) -> WVResult {
     println!("should retrieve url {}", url);
 
     wv.eval(&format!("updateResult('{}')", "hello world"))
-
-    // web_view::builder()
-    //     .title("Roogle")
-    //     .content(Content::Url(url))
-    //     .size(800, 600)
-    //     .resizable(true)
-    //     .debug(true)
-    //     .user_data(())
-    //     .invoke_handler(|webview, arg| {
-    //         use Cmd::*;
-    //         match serde_json::from_str(arg).unwrap() {
-    //             Init => println!("this would be the init handler"),
-    //             Log { text } => println!("{}", text),
-    //             FetchUrl { text } => {
-    //                 println!("{}", text);
-    //                 open_url(webview, text)
-    //             }
-    //         }
-    //         Ok(())
-    //     }).run()
-    //     .unwrap();
 }
