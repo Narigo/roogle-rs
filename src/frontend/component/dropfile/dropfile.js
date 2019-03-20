d7b7f55a56de56b3ac3f40fba24137ed22143649
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-03-20 21:40:00 +0100

    Style dropfile and put drop handler in separate function
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/frontend/component/dropfile/dropfile.js
+++ src/frontend/component/dropfile/dropfile.js
@@ -3,14 +3,51 @@ import cn from "classnames";
 import style from "./dropfile.scss";
 import useRustCommand from "../../hooks/use-rust-command";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 const Dropfile = ({ onDrop }) => {
   const [dragOver, setDragOver] = useState(false);
   const log = useRustCommand("log");
   console.log("dropfile render");
+  const onDropHandler = event => {
+    event.preventDefault();
+    const files = [];
+    if (event.dataTransfer.items) {
+      for (let i = 0; i < event.dataTransfer.items.length; i++) {
+        if (event.dataTransfer.items[i].kind === "file") {
+          files.push(event.dataTransfer.items[i].getAsFile());
+        }
+      }
+    } else {
+      for (let i = 0; i < event.dataTransfer.files.length; i++) {
+        files.push(event.dataTransfer.files[i]);
+      }
+    }
+    Promise.all(
+      files.map(
+        file =>
+          new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = function(event) {
+              resolve(event.target.result);
+            };
+            reader.onerror = function(error) {
+              reject(error);
+            };
+            reader.readAsArrayBuffer(file);
+          })
+      )
+    )
+      .then(files => onDrop(files))
+      .catch(err => log(`Error while reading file: ${err}`));
+  };
+
   return (
     <Grid
       className={cn(style.root, dragOver && style.dragOver)}
+      container
+      alignItems="center"
+      justify="center"
       onClick={() => log("something clicked.")}
       onDragOver={event => {
         event.preventDefault();
@@ -20,40 +57,9 @@ const Dropfile = ({ onDrop }) => {
         event.preventDefault();
         setDragOver(false);
       }}
-      onDrop={event => {
-        event.preventDefault();
-        const files = [];
-        if (event.dataTransfer.items) {
-          for (let i = 0; i < event.dataTransfer.items.length; i++) {
-            if (event.dataTransfer.items[i].kind === "file") {
-              files.push(event.dataTransfer.items[i].getAsFile());
-            }
-          }
-        } else {
-          for (let i = 0; i < event.dataTransfer.files.length; i++) {
-            files.push(event.dataTransfer.files[i]);
-          }
-        }
-        Promise.all(
-          files.map(
-            file =>
-              new Promise((resolve, reject) => {
-                const reader = new FileReader();
-                reader.onload = function(event) {
-                  resolve(event.target.result);
-                };
-                reader.onerror = function(error) {
-                  reject(error);
-                };
-                reader.readAsArrayBuffer(file);
-              })
-          )
-        )
-          .then(files => onDrop(files))
-          .catch(err => log(`Error while reading file: ${err}`));
-      }}
+      onDrop={onDropHandler}
     >
-      Drop a PDF file here
+      <Typography>Drop a PDF file here</Typography>
     </Grid>
   );
 };
