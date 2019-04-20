5fadb9c54e55cb768a98d39140443e1172b7e940
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-04-20 22:22:51 +0200

    Fix probabilities
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/frontend/component/results/results.js
+++ src/frontend/component/results/results.js
@@ -7,23 +7,26 @@ import Tabs from "@material-ui/core/Tabs";
 import Result from "../result/result";
 
 const textsReducer = (state, action) => {
-  const newState = state;
-  newState[action.index].maxProbability = action.probability;
-  return newState;
+  switch (action.type) {
+    case "set-probability":
+      const { index, maxProbability } = action.payload;
+      return [...state.slice(0, index), { ...state[index], maxProbability }, ...state.slice(index + 1)];
+  }
+  return state;
 };
 
 const Results = ({ sentences }) => {
   const [currentTab, setCurrentTab] = useState(0);
   const [texts, dispatch] = useReducer(
     textsReducer,
-    sentences.map(sentence => ({ label: `${sentence.slice(0, 25)}...`, maxProbability: 0 }))
+    sentences.map(sentence => ({ label: sentence, maxProbability: 0 }))
   );
   return (
     <Fragment>
       <AppBar position="static">
         <Tabs value={currentTab} onChange={(_event, value) => setCurrentTab(value)} variant="fullWidth">
           {texts.map((text, index) => (
-            <Tab key={index} label={text.label} />
+            <Tab key={index} label={`${text.label.slice(0, 25)} (${text.maxProbability})`} />
           ))}
         </Tabs>
       </AppBar>
@@ -32,10 +35,12 @@ const Results = ({ sentences }) => {
           key={sentence}
           sentence={sentence}
           show={currentTab === index}
-          onResult={res => {
-            console.log("got a result");
-            dispatch({ index, probability: res.maxProbability });
-          }}
+          onResult={res =>
+            dispatch({
+              type: "set-probability",
+              payload: { index, maxProbability: res.maxProbability }
+            })
+          }
         />
       ))}
     </Fragment>
