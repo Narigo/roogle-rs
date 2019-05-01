5899764cb5d07cef8d7fe102cdca1176042bb429
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-05-02 00:21:20 +0200

    Add div to SearchItem to get root styling back
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/frontend/component/search-item/search-item.js
+++ src/frontend/component/search-item/search-item.js
@@ -5,14 +5,16 @@ import Typography from "@material-ui/core/Typography";
 import style from "./search-item.scss";
 
 const SearchItem = ({ item, onClick }) => (
-  <Paper className={style.paper} onClick={onClick}>
-    <Typography paragraph>
-      {item.url} (Probability {item.probability}%)
-    </Typography>
-    <Typography paragraph className={style.description}>
-      <div dangerouslySetInnerHTML={{ __html: item.description }} />
-    </Typography>
-  </Paper>
+  <div className={style.root}>
+    <Paper className={style.paper} onClick={onClick}>
+      <Typography paragraph>
+        {item.url} (Probability {item.probability}%)
+      </Typography>
+      <Typography paragraph className={style.description}>
+        <div dangerouslySetInnerHTML={{ __html: item.description }} />
+      </Typography>
+    </Paper>
+  </div>
 );
 
 export default SearchItem;
