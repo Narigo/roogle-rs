0d84f705bed6a974e5da1cc92a15514a2efa1c31
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-05-04 23:45:45 +0200

    Add probability test for higher probabilities
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/frontend/component/search-item/search-item.js
+++ src/frontend/component/search-item/search-item.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import cn from "classnames";
 
 import style from "./search-item.scss";
 
 const SearchItem = ({ item, onClick }) => (
   <div className={style.root}>
-    <Paper className={style.paper} onClick={onClick}>
+    <Paper
+      className={cn(style.paper, { [style.probably]: item.probability > 20, [style.check]: item.probability > 50 })}
+      onClick={onClick}
+    >
       <Typography paragraph>
         {item.url} (Probability {item.probability}%)
       </Typography>
