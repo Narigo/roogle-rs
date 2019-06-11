11d25759d875f2e1aeefd652dd70742449c43cf6
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-06-11 02:55:51 +0200

    Add first tests and fix setup
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/frontend/lib/get-google-url.js
+++ src/frontend/lib/get-google-url.js
@@ -1,3 +1,6 @@
 export default function getGoogleUrl(sentence) {
+  if (!sentence || sentence === "") {
+    throw new Error("'sentence' argument to getGoogleUrl needs to be a string with a value");
+  }
   return `https://www.google.de/search?q=${encodeURIComponent(sentence)}`;
 }
--- src/frontend/lib/get-google-url.spec.js
+++ src/frontend/lib/get-google-url.spec.js
@@ -1,5 +1,19 @@
+import getGoogleUrl from "./get-google-url";
+
 describe("get-google-url", () => {
-  it("test setup works", () => {
-    expect(true);
+  it("throws an error on no argument", () => {
+    expect(() => getGoogleUrl()).toThrow();
+  });
+
+  it("throws an error when there is an empty string as argument", () => {
+    expect(() => getGoogleUrl("")).toThrow();
+  });
+
+  it("returns the proper URL", () => {
+    expect(getGoogleUrl("This is a test of the basic functionality")).toMatchSnapshot();
+  });
+
+  it("returns the proper URL when Umlaute are used", () => {
+    expect(getGoogleUrl("Das ist ein Sätzchen mit Umlauten dass das mal getestet wird")).toMatchSnapshot();
   });
 });
