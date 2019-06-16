8d75af6794ce9b723b11cf2851421d48f90915a1
Joern Bernhardt <joern.bernhardt@googlemail.com>
2019-06-16 12:40:22 +0200

    Remove unused fetch_url code and dependencies
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,7 @@
 extern crate base64;
 #[macro_use]
 extern crate serde_derive;
-extern crate reqwest;
 extern crate serde_json;
-extern crate url;
 extern crate web_view;
 extern crate webbrowser;
 
@@ -19,7 +17,8 @@ fn main() {
                 .content(Content::Url(format!(
                     "http://{}:{}/index.html",
                     server.domain, server.port
-                ))).size(800, 600)
+                )))
+                .size(800, 600)
                 .resizable(true)
                 .debug(true)
                 .user_data(())
@@ -28,17 +27,16 @@ fn main() {
                     match serde_json::from_str(arg).unwrap() {
                         Init => {}
                         Log { text } => println!("{}", text),
-                        FetchUrl { text } => {
-                            fetch_url(webview, text)?;
-                        }
                         OpenUrl { text } => {
                             open_url(webview, text);
                         }
                     }
                     Ok(())
-                }).run()
+                })
+                .run()
                 .unwrap();
-        }).unwrap();
+        })
+        .unwrap();
 }
 
 #[derive(Deserialize)]
@@ -46,23 +44,7 @@ fn main() {
 pub enum Cmd {
     Init,
     Log { text: String },
-    FetchUrl { text: String },
     OpenUrl { text: String },
-}
-
-fn fetch_url<T>(wv: &mut WebView<T>, url: String) -> WVResult {
-    let body = reqwest::get(url.as_str())
-        .map(|mut x| x.text())
-        .unwrap_or(Ok(String::from("failed")))
-        .unwrap();
-
-    let result = format!(
-        "updateResult['{}']('{}')",
-        url,
-        base64::encode(body.as_bytes())
-    );
-
-    wv.eval(&result)
 }
 
 fn open_url<T>(_wv: &mut WebView<T>, url: String) {
